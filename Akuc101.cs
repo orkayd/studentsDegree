71c1c9aee20727bc0703208946a858386c992f98
orkayd <48044467+orkayd@users.noreply.github.com>
2021-08-29 00:29:11 +0300

    Add files via upload

---
+++ Akuc101.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AKUc101
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many students?");
+            int ST = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i<=ST;i++)
+            {
+                Console.WriteLine("Name");
+                var Name = Console.ReadLine();
+
+                Console.WriteLine("Degree");
+                int De = Convert.ToInt32(Console.ReadLine());
+
+
+                if (De >= 60)
+                {
+                    Console.WriteLine(Name + "" + De + "" + "Passed");
+
+                }
+                else
+                {
+                    Console.WriteLine(Name +""+ De+ ""+ "Failed");
+                }
+            }
+
+
+            
+
+
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
